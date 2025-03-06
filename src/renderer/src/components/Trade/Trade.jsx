cb7e87a4368e6410b3b4b4c396e654ffd089500b
DmiTsa <dmitrytaskaev.dt@gmail.com>
2025-03-06 18:57:09 +0300

    lots

--- src/renderer/src/components/Trade/Trade.jsx
+++ src/renderer/src/components/Trade/Trade.jsx
@@ -6,10 +6,10 @@ function Trade() {
     <div id="trade" className="p-2">
       <div id="tradeInfo" className="flex items-center">
         <div id="info" className="flex flex-1 flex-col">
-          <h2 className="text-lg font-bold">
+          <h2 className="h2 font-bold">
             Торги № ВФ/СМР/Связь/23/2025 (Переговоры со снижением) IceTrade №2025-3451212
           </h2>
-          <h3>
+          <h3 className="h3">
             Подача предложений: 02.03.2025 - 10.03.2025, вскрытие конвертов с предложениями:
             10.03.2025, процедура снижения: 12.03.2025
           </h3>
@@ -84,21 +84,243 @@ function Trade() {
       </div>
 
       <div id="stageInfo" className="py-4">
-        <ul className="flex gap-1">
-          <li className="stageInfoItem bg-blue-400">Подготовка</li>
-          <li className="stageInfoItem bg-yellow-300 activeStageInfo">Прием предложений</li>
-          <li className="stageInfoItem bg-yellow-300 uncomplete">Допуск</li>
-          <li className="stageInfoItem  bg-green-400 uncomplete">Снижение</li>
-          <li className="stageInfoItem  bg-green-400 uncomplete">Закрытие(договор)</li>
-          <li className="stageInfoItem bg-gray-400 uncomplete">Оформление</li>
-        </ul>
+        <div className="flex gap-1">
+          <div id="stageItem" className="stageInfoItem bg-blue-400 activeStageInfo">
+            <p>Подготовка</p>
+            <div id="stageOptions" className="flex gap-2">
+              <button className="flex gap-2 items-center text-sm cursor-pointer hover:underline py-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12"
+                  />
+                </svg>
+                Приглашение
+              </button>
+
+              <button className="flex gap-2 items-center text-sm cursor-pointer hover:underline py-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                  />
+                </svg>
+                Опубликовать
+              </button>
+
+              <button className="flex gap-2 items-center text-sm cursor-pointer hover:underline py-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                  />
+                </svg>
+                Пригласить
+              </button>
+            </div>
+          </div>
+
+          <div className="stageInfoItem bg-yellow-300 uncomplete">
+            Прием предложений и вскрытие конвертов
+          </div>
+          <div className="stageInfoItem bg-yellow-300 uncomplete">Допуск</div>
+          <div className="stageInfoItem bg-green-400 uncomplete">Снижение</div>
+          <div className="stageInfoItem bg-green-400 uncomplete">Закрытие(договор)</div>
+          <div className="stageInfoItem bg-gray-400 uncomplete">Оформление</div>
+        </div>
       </div>
+
+      <div id="lots">
+        <p className="h3 font-bold">Лоты для торгов:</p>
+
+        <div id="lot" className="flex border rounded p-1 m-1 relative">
+          <p id="nameLot" className="mx-2 font-bold">
+            Лот №1
+          </p>
+
+          <div id="lotInfo" className="flex flex-col">
+            <p className="underline">
+              Реконструкция сети связи в поселки Нижние Пиздюки. Возведение строительных
+              конструкция, прокладка сети ХПОН
+            </p>
+
+            <p className="flex">
+              Стоимость - 2 993 904, 02 BYN., строительство - 12.11.2025 - 30.11.2025, ПОС - 12 мес.
+            </p>
+
+            {/* <ul className="flex">
+              <li>Победитель</li>
+              <li>Цена</li>
+              <li>Снижение %</li>
+              <li>Снижение руб</li>
+            </ul> */}
+          </div>
+
+          <div id="buttonsLot" className="absolute top-0 right-0 m-1 flex gap-1">
+            <button className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
+                />
+              </svg>
+            </button>
+
+            <button className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                />
+              </svg>
+            </button>
+
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div id="lot" className="flex border rounded p-1 m-1 relative">
+          <p id="nameLot" className="mx-2 font-bold">
+            Лот №2
+          </p>
+
+          <div id="lotInfo" className="flex flex-col">
+            <p className="underline">
+              Реконструкция сети связи в поселки Нижние Пиздюки. Возведение строительных
+              конструкция, прокладка сети ХПОН
+            </p>
+
+            <p className="flex">
+              Стоимость - 2 993 904, 02 BYN., строительство - 12.11.2025 - 30.11.2025, ПОС - 12 мес.
+            </p>
+
+            {/* <ul className="flex">
+              <li>Победитель</li>
+              <li>Цена</li>
+              <li>Снижение %</li>
+              <li>Снижение руб</li>
+            </ul> */}
+          </div>
+
+          <div id="buttonsLot" className="absolute top-0 right-0 m-1 flex gap-1">
+            <button className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
+                />
+              </svg>
+            </button>
+
+            <button className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                />
+              </svg>
+            </button>
+
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div id="lots">
+        <p className="h3 font-bold">Предложения подрядчиков:</p>
+      </div>
+
+      <p>предложения</p>
+      <p>панель протоколов</p>
 
       <p>приказ</p>
       <p>комиссия</p>
-      <p>лоты</p>
-      <p>предложения</p>
-      <p>панель протоколов</p>
     </div>
   )
 }
